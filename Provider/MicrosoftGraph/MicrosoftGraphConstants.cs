71f01e3a40f94dbcbde50c48948956cae4a25c17
Sandro Castillo <Sandro.Castillo@ysoft.com>
2024-02-27 17:11:34 -0500

    work

--- Provider/MicrosoftGraph/MicrosoftGraphConstants.cs
+++ Provider/MicrosoftGraph/MicrosoftGraphConstants.cs
@@ -7,5 +7,15 @@ namespace mail_library.Provider.MicrosoftGraph {
         public const string DEFAULT_AUTHORITY_FORMAT = "https://login.microsoftonline.com/{0}/";
         public static readonly string[] DEFAULT_SCOPES = { "https://graph.microsoft.com/Mail.ReadWrite", "https://graph.microsoft.com/Mail.Send", "https://graph.microsoft.com/User.Read" };
 
+        /// <summary>
+        /// Expected error on empty cache for AcquireTokenSilent flow
+        /// </summary>
+        internal const string ERR_MSAL_EMPTY = "No account or login hint was passed to the AcquireTokenSilent call";
+
+        public enum AuthenticationAlternativeOption {
+            None,
+            DeviceCode,
+            Browser
+        }
     }
 }
--- Provider/MicrosoftGraph/MicrosoftGraphProvider.cs
+++ Provider/MicrosoftGraph/MicrosoftGraphProvider.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.Extensions.Logging;
+using Microsoft.Identity.Client;
+using Microsoft.Identity.Client.Extensions.Msal;
 using static mail_library.Provider.MicrosoftGraph.MicrosoftGraphConstants;
 
 namespace mail_library.Provider.MicrosoftGraph {
@@ -14,6 +16,13 @@ namespace mail_library.Provider.MicrosoftGraph {
         public string CacheFileName { get; set; } = DEFAULT_CACHE_FILENAME;
         public string[] Scopes { get; set; } = DEFAULT_SCOPES;
         public string Authority { get; set; } = string.Format(DEFAULT_AUTHORITY_FORMAT, DEFAULT_TENANT);
+        public AuthenticationAlternativeOption AuthenticationAlternative { get; set; } = AuthenticationAlternativeOption.None;
+
+        public Func<DeviceCodeResult, Task> DeviceCodeCallback { get; set; } = deviceCode => {
+            Console.WriteLine(deviceCode.Message);
+
+            return Task.FromResult(0);
+        };
 
         /// <summary>
         /// Connects to Microsoft Graph Mail endpoint on behalf of a User
@@ -60,8 +69,64 @@ namespace mail_library.Provider.MicrosoftGraph {
             throw new NotImplementedException();
         }
 
-        public async Task<Microsoft.Graph.GraphServiceClient> CreateGraphClientAsync(bool interactive, CancellationToken cancellationToken) {
-            throw new NotImplementedException();
+        public async Task<Microsoft.Graph.GraphServiceClient> CreateGraphClientAsync(string accessToken, CancellationToken cancellationToken) {
+            Microsoft.Graph.GraphServiceClient client = new(BaseUrl, new Microsoft.Graph.DelegateAuthenticationProvider(async (requestMessage) => {
+                requestMessage.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("bearer", accessToken);
+            }));
+
+            return await Task.FromResult(client);
+        }
+
+        public async Task<Microsoft.Graph.GraphServiceClient> CreateGraphClientAsync(CancellationToken cancellationToken) {
+
+            var accessToken = await GetAccessTokenAsync(cancellationToken);
+
+            return await CreateGraphClientAsync(accessToken, cancellationToken);
+        }
+
+        private async Task<string> GetAccessTokenAsync(CancellationToken cancellationToken) {
+            var app = await BuildPublicClientAsync();
+
+            AuthenticationResult authentication;
+            try {
+                var accounts = await app.GetAccountsAsync();
+                authentication = await app.AcquireTokenSilent(Scopes, accounts.FirstOrDefault()).ExecuteAsync(cancellationToken);
+
+                return authentication.AccessToken;
+
+            } catch when (AuthenticationAlternative == AuthenticationAlternativeOption.DeviceCode) {
+
+                authentication = await app.AcquireTokenWithDeviceCode(Scopes, DeviceCodeCallback).ExecuteAsync(cancellationToken);
+
+                return authentication.AccessToken;
+
+            } catch when (AuthenticationAlternative == AuthenticationAlternativeOption.Browser) {
+
+                authentication = await app.AcquireTokenInteractive(Scopes).ExecuteAsync(cancellationToken);
+
+            } catch (MsalUiRequiredException ex) when (ex.Message.Contains("", StringComparison.InvariantCultureIgnoreCase)) {
+                // IGNORE
+            }
+
+            return string.Empty;
+        }
+
+        private async Task<IPublicClientApplication> BuildPublicClientAsync() {
+            IPublicClientApplication pca = PublicClientApplicationBuilder.Create(ClientId)
+                .WithTenantId(TenantId)
+                .WithRedirectUri(RedirectUri)
+                .WithAuthority(Authority)
+                .Build();
+
+            var storageProperties = new StorageCreationPropertiesBuilder(CacheFileName, CacheFolder)
+                .WithLinuxUnprotectedFile() // TO-DO: Review options on linux
+                .Build();
+
+            var cacheHelper = await MsalCacheHelper.CreateAsync(storageProperties);
+            cacheHelper.RegisterCache(pca.UserTokenCache);
+
+            return pca;
         }
     }
+
 }
